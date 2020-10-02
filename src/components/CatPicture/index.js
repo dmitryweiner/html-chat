08bcb719cc4b86c66ea7dee17e99ca8d31dead63
Dmitry Weiner <dmitry.weiner@gmail.com>
2020-10-02 11:18:13 +0700

    Use styles

--- src/components/CatPicture/index.js
+++ src/components/CatPicture/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import cat from './cat.jpg';
+import styles from './styles.css';
+
+/**
+ * For incapsulated styles see
+ * @see https://medium.com/@epilande/working-with-css-modules-a6b8aad37f3a
+ */
 
 export default () => {
-    return <img src={cat} />;
+    return <img src={cat} className="cat-picture" />;
 };
