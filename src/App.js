288b21ac8dbad2e9d674b3d7a397b9a4b4a2a0b7
Dmitry Weiner <dmitry.weiner@gmail.com>
2020-10-23 17:35:28 +0700

    Adding links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
 import LoginView from './views/LoginView';
 import RegistrationView from './views/RegistrationView';
 import ChatView from './views/ChatView';
@@ -8,11 +8,14 @@ class App extends React.Component {
     render() {
         return (
             <>
+                <Link to="/login">Логин</Link>&nbsp;
+                <Link to="/registration">Регистрация</Link>&nbsp;
+                <Link to="/chat">Чат</Link>
                 <Switch>
                     <Route path="/login" component={LoginView} />
                     <Route path="/registration" component={RegistrationView} />
                     <Route path="/chat" component={ChatView} />
-                    <Redirect from="/" to="/login" />
+                    <Redirect exact from="/" to="/login" />
                 </Switch>
             </>
         );
