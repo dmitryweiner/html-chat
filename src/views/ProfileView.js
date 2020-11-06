f170ba42485ef5a1ddd153b2398e20873353b599
dmitryweiner <dmitry.weiner@gmail.com>
2020-11-06 14:00:52 +0700

    Added chat form

--- src/views/ProfileView.js
+++ src/views/ProfileView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import apiService from '../apiService';
+import ChatForm from '../components/ChatForm';
 
 export default class ProfileView extends React.Component {
     constructor(props) {
@@ -26,6 +27,8 @@ export default class ProfileView extends React.Component {
                         <div>Создан: {new Date(this.state.user.createdAt).toLocaleString()}</div>
                     </>
                 )}
+
+                <ChatForm handleSubmit={data => console.log(data)} />
             </>
         );
     }
