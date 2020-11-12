a56dfb27024920e9d6b52cbc449204d996474abc
dmitryweiner <dmitry.weiner@gmail.com>
2020-11-12 15:02:54 +0700

    Updated chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -7,24 +7,63 @@ import PropTypes from 'prop-types';
 class Chat extends React.Component {
     innerClickHandle(e) {
         e.preventDefault();
-        this.props.clickHandle(this.props.id);
+        this.props.goHandle(this.props.chat.id);
+    }
+
+    isOwner() {
+        return this.props.chat.userId === this.props.userId;
+    }
+
+    isParticipant() {
+        return this.props.chat.participants.includes(this.props.userId);
+    }
+
+    renderChat() {
+        if (this.isOwner()) {
+            return (
+                <>
+                    <a href="/" onClick={e => this.innerClickHandle(e)}>
+                        {this.props.chat.title}
+                    </a>
+                    <button onClick={() => this.props.deleteHandle(this.props.chat.id)}>
+                        удалить
+                    </button>
+                </>
+            );
+        }
+
+        if (this.isParticipant()) {
+            return (
+                <a href="/" onClick={e => this.innerClickHandle(e)}>
+                    {this.props.chat.title}
+                </a>
+            );
+        }
+
+        return (
+            <>
+                <span>{this.props.chat.title}</span>
+                <button onClick={() => this.props.joinHandle(this.props.chat.id)}>вступить</button>
+            </>
+        );
     }
 
     render() {
-        return (
-            <li>
-                <a href="/" onClick={e => this.innerClickHandle(e)}>
-                    {this.props.title}
-                </a>
-            </li>
-        );
+        return <li>{this.renderChat()}</li>;
     }
 }
 
 Chat.propTypes = {
-    id: PropTypes.string,
-    title: PropTypes.string,
-    clickHandle: PropTypes.func
+    userId: PropTypes.string.isRequired,
+    chat: PropTypes.shape({
+        id: PropTypes.string,
+        title: PropTypes.string,
+        participants: PropTypes.arrayOf(PropTypes.string),
+        createdAt: PropTypes.string
+    }).isRequired,
+    goHandle: PropTypes.func,
+    joinHandle: PropTypes.func,
+    deleteHandle: PropTypes.func
 };
 
 export default Chat;
--- src/stories/Chat.stories.js
+++ src/stories/Chat.stories.js
@@ -9,9 +9,38 @@ export default {
 
 const Template = args => <Chat {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-    id: '853d59e4a2b8e',
-    title: 'Мой первый чат',
-    clickHandle: action('clicked!')
+const chat = {
+    id: '0b634621ac63b',
+    createdAt: '2020-11-11T11:16:03.9013Z',
+    title: 'Мой супер чат',
+    userId: 'c5acc3193e83b',
+    participants: ['c5acc3193e83b', 'db6bb9ac9d72f'],
+    isPrivate: false
+};
+
+export const ChatOwner = Template.bind({});
+ChatOwner.args = {
+    userId: 'c5acc3193e83b',
+    chat,
+    goHandle: action('go'),
+    deleteHandle: action('delete'),
+    joinHandle: action('join')
+};
+
+export const ChatParticipant = Template.bind({});
+ChatParticipant.args = {
+    userId: 'db6bb9ac9d72f',
+    chat,
+    goHandle: action('go'),
+    deleteHandle: action('delete'),
+    joinHandle: action('join')
+};
+
+export const NotParticipant = Template.bind({});
+NotParticipant.args = {
+    userId: 'fd156gsd6fg',
+    chat,
+    goHandle: action('go'),
+    deleteHandle: action('delete'),
+    joinHandle: action('join')
 };
